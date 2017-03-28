65cf3f06005f659edc023c7b98f8459582af3fc6
Caudrelier <tazukay@hotmail.fr>
2017-03-28 21:50:04 +0200

    Corriged the suppression data script

--- app/sql/suppression_tables.sql
+++ app/sql/suppression_tables.sql
@@ -1,2 +1,3 @@
-drop view vin_view;
-drop table if exists note, critere, assemblage, vin, impact, evenement, traite, traitement, exploitation, parcelle, cepage;
+drop type if exists EtypeSol, Eexposition, EmodeCulture cascade;
+drop view  if exists vin_view cascade;
+drop table if exists note, critere, assemblage, vin, impact, evenement, traite, traitement, exploitation, parcelle, cepage cascade;

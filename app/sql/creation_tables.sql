4b054affa1a77577b3dc3fa018220efba5eb8790
Corentin <ch2kn@free.fr>
2017-03-31 19:01:06 +0200

    Change vin primary key

--- app/sql/creation_tables.sql
+++ app/sql/creation_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE parcelle (
 			exposition Eexposition,
 			cepage_nom varchar(255) REFERENCES cepage(nom) ON UPDATE CASCADE ON DELETE CASCADE,
 			CHECK (surface >= 0)
-			); 
+			);
 CREATE TABLE exploitation (
 			parcelle_nom varchar(255) REFERENCES parcelle (nom) ON UPDATE CASCADE ON DELETE CASCADE,
 			annee int NOT NULL,
@@ -41,29 +41,34 @@ CREATE TABLE impact (
 			PRIMARY KEY (exploitation_parcelle, evenement_type, date)
 			);
 CREATE TABLE vin (
-			nom varchar(255) PRIMARY KEY,
+			nom varchar(255),
 			prix NUMERIC(10, 2) NOT NULL,
-			CHECK (prix >= 0)
+			annee int,
+			CHECK (prix >= 0),
+			PRIMARY KEY(nom, annee)
 			);
 CREATE TABLE assemblage (
 			pourcentage NUMERIC(5, 2) NOT NULL DEFAULT 100,
 			exploitation_annee int,
 			exploitation_parcelle varchar(255),
 			FOREIGN KEY (exploitation_annee, exploitation_parcelle) REFERENCES exploitation (annee,parcelle_nom) ON UPDATE CASCADE ON DELETE CASCADE,
-			vin_nom VARCHAR(255) REFERENCES vin (nom) ON UPDATE CASCADE ON DELETE CASCADE,
-			PRIMARY KEY (vin_nom, exploitation_parcelle, exploitation_annee),
+			vin_nom VARCHAR(255),
+			vin_annee int,
+			FOREIGN KEY (vin_nom, vin_annee) REFERENCES vin (nom, annee) ON UPDATE CASCADE ON DELETE CASCADE,
+			PRIMARY KEY (vin_nom, vin_annee, exploitation_parcelle, exploitation_annee),
 			CHECK (pourcentage > 0 AND pourcentage <= 100)
-			); 
+			);
 CREATE TABLE critere (
 			nom varchar(255) PRIMARY KEY
 			);
 CREATE TABLE note (
 			note int NOT NULL CHECK(note >= 0 AND note <= 20),
 			critere_nom VARCHAR(255) REFERENCES critere(nom) ON UPDATE CASCADE ON DELETE CASCADE,
-			vin_nom  VARCHAR(255) REFERENCES vin(nom) ON UPDATE CASCADE ON DELETE CASCADE,
-			annee int NOT NULL,
-			PRIMARY KEY(critere_nom, vin_nom)
+			vin_nom VARCHAR(255),
+			vin_annee int,
+			FOREIGN KEY (vin_nom, vin_annee) REFERENCES vin (nom, annee) ON UPDATE CASCADE ON DELETE CASCADE,
+			PRIMARY KEY(critere_nom, vin_nom, vin_annee)
 			);
 
 CREATE VIEW vin_view AS
-	SELECT vin.*, ROUND(AVG(note), 2) as note FROM vin LEFT JOIN note ON vin.nom = note.vin_nom GROUP BY vin.nom;
+	SELECT vin.*, ROUND(AVG(note), 2) as note FROM vin LEFT JOIN note ON vin.nom = note.vin_nom AND vin.annee = note.vin_annee GROUP BY vin.nom, vin.annee;
